f762f5989ff75d1e8b06e3d1058d485a7e9d40ef
donnyangel.caballero <dogncamo@suramericana.com.co>
2024-03-10 23:03:37 -0500

    added books module

--- src/books/model/book.input.ts
+++ src/books/model/book.input.ts
@@ -1,8 +1,29 @@
-import { IsObject } from 'class-validator'
+import { IsNumber, IsObject, IsString } from 'class-validator'
 import { IBook } from './book.types'
 export class CreateBookInput {
+  @IsString()
+  title: string
+  
+  @IsNumber()
+  pages: number
+
+  @IsString()
+  genre: string
+
+  @IsString()
+  cover: string
+
+  @IsString()
+  synopsis: string
+
+  @IsNumber()
+  year: number
+
+  @IsString()
+  ISBN: string
+
   @IsObject()
-  book: IBook
+  author: object
 }
 
 export class UpdateBookInput extends CreateBookInput{}
